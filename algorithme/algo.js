c31aad190fa78f09706130b805fe2b37eaf7366c
hazem juini <hazemjuini243@gmail.com>
2023-06-20 10:16:48 +0100

    algorithme

---
+++ algorithme/algo.js
@@ -0,0 +1,8 @@
+var countPositives = 0;
+var numbres = [3,4,-2,7,16,-8,0];
+for(var i = 0; i<numbres.length; i++){
+if(numbres[i]>0){
+    [countPositives++]
+}
+}
+    console.log("there are" + countPositives + "positive values");
