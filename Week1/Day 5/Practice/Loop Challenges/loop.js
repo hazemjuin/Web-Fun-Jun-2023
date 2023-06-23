6750f8f733afd8eb947e0f87fe7809c2075eb8f7
hazem juini <hazemjuini243@gmail.com>
2023-06-23 11:27:50 +0100

    practice

---
+++ Week1/Day 5/Practice/Loop Challenges/loop.js
@@ -0,0 +1,54 @@
+// loop 1
+
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+    console.log(i);
+    }
+}
+
+
+// loop 2
+
+for (var i = 100; i >= 0; i--) {
+    if (i % 3 === 0) {
+    console.log(i);
+    }
+}
+
+
+// loop 3 
+
+const sequence = [4, 2.5, 1, -0.5, -2, -3.5];
+
+for (var i = 0; i < sequence.length; i++) {
+    console.log(sequence[i]);
+}
+
+
+// loop 4
+
+let sum = 0;
+
+for (var i = 1; i <= 100; i++) {
+sum += i;
+}
+
+console.log(sum);
+
+
+// loop 5
+
+var product = 1;
+
+for (var i = 1; i <= 12; i++) {
+  product *= i;
+}
+
+console.log(product);
+
+
+
+
+
+
+
